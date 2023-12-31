98b59c795a2f93adcfa1a5600c2903202500ab24
Biniam Zemedkun <46942052+bzemedku98@users.noreply.github.com>
2023-12-31 14:59:02 -0500

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Note from "./Note";
 import CreateArea from "./CreateArea";
+import Note from "./Note";
 
 function App() {
   const [notes, setNotes] = useState([]);
